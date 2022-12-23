c7d4876117885851711e47d8e9dea20bb172dad6
Pedro Germano <pedrogermano232@gmail.com>
2022-12-23 18:33:20 -0300

    done

--- crashCourseDeved/crashCourse/main.js
+++ crashCourseDeved/crashCourse/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import "./style.css"
 
@@ -34,10 +35,15 @@ scene.add(camera)
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
 renderer.render(scene, camera)
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
 
 // Resize
@@ -51,7 +57,28 @@ window.addEventListener('resize', () => {
 })
 
 const loop = () => {
+  controls.update()
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop)
 }
 loop()
+
+const tl = gsap.timeline({defaults: {duration: 1 }})
+tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z: 1, x: 1, y: 1 })
+tl.fromTo('nav', {y: '-100%'}, {y: '0%'})
+tl.fromTo('.title', {opacity: 0 }, {opacity: 1 })
+
+
+let mouseDown = false
+let rgb = [];
+window.addEventListener('mouseDown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes))
+    ]
+  }
+})
